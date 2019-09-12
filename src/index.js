13f60eb032a1e1d44364a1cf9336684cfdf45083
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-12 17:57:38 -0400

    add skeleton starter code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// new file
+// import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a react component
+const App = () => {
+  return <div>Hello there!</div>
+}
+
+// take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
