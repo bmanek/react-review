1fc184749ce9faa473d29b23296db07d57912132
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-13 15:41:59 -0400

    add starter code to index.js and link semantic-ui to styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // clean slate
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello there!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
