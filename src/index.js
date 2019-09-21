e7389d8e9a80fabcbb5dd9784dd74607f8650cdf
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-20 22:52:11 -0400

    add ApprovalCard with hardcoded content, reformat CommentDetail props presentation in index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Marcus" time="15 minutes ago" avatar={faker.image.avatar()}  commentText="Cool!"/>
-      <CommentDetail author="Adrian" time="22 minutes ago" avatar={faker.image.avatar()} commentText="Nice stuff!"/>
-      <CommentDetail author="Lucille" time="38 minutes ago" avatar={faker.image.avatar()} commentText="Dig it!"/>
-      <CommentDetail author="Jerome" time="58 minutes ago" avatar={faker.image.avatar()} commentText="Cool!"/>
-      <CommentDetail author="Marnice" time="4 hours ago" avatar={faker.image.avatar()} commentText="Dope!"/>
-      <CommentDetail author="Alex" time="2 days ago" avatar={faker.image.avatar()} commentText="Cool!"/>
+      <ApprovalCard />
+      <CommentDetail
+        author="Marcus"
+        time="15 minutes ago"
+        avatar={faker.image.avatar()}  commentText="Cool!"
+      />
+      <CommentDetail
+        author="Adrian"
+        time="22 minutes ago"
+        avatar={faker.image.avatar()} commentText="Nice stuff!"
+      />
+      <CommentDetail
+        author="Lucille"
+        time="38 minutes ago"
+        avatar={faker.image.avatar()} commentText="Dig it!"
+      />
+      <CommentDetail
+        author="Jerome"
+        time="58 minutes ago"
+        avatar={faker.image.avatar()} commentText="Cool!"
+      />
+      <CommentDetail
+        author="Marnice"
+        time="4 hours ago"
+        avatar={faker.image.avatar()} commentText="Dope!"
+      />
+      <CommentDetail
+        author="Alex"
+        time="2 days ago"
+        avatar={faker.image.avatar()} commentText="Cool!"
+      />
     </div>
   )
 }
