34b2d06a0d8489f7a32448f8326a421fc9ae6e0e
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-20 22:39:14 -0400

    add props for handling time, commentText, and avatar

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Marcus" />
-      <CommentDetail author="Adrian" />
-      <CommentDetail author="Lucille" />
-      <CommentDetail author="Jerome" />
-      <CommentDetail author="Marnice" />
-      <CommentDetail author="Alex" />
+      <CommentDetail author="Marcus" time="15 minutes ago" avatar={faker.image.avatar()}  commentText="Cool!"/>
+      <CommentDetail author="Adrian" time="22 minutes ago" avatar={faker.image.avatar()} commentText="Nice stuff!"/>
+      <CommentDetail author="Lucille" time="38 minutes ago" avatar={faker.image.avatar()} commentText="Dig it!"/>
+      <CommentDetail author="Jerome" time="58 minutes ago" avatar={faker.image.avatar()} commentText="Cool!"/>
+      <CommentDetail author="Marnice" time="4 hours ago" avatar={faker.image.avatar()} commentText="Dope!"/>
+      <CommentDetail author="Alex" time="2 days ago" avatar={faker.image.avatar()} commentText="Cool!"/>
     </div>
   )
 }
