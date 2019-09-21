c5a86196fd4b7c8ee4464f153320680bd0eb306f
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-20 23:21:52 -0400

    add ApprovalCard wrapping for comments

--- src/index.js
+++ src/index.js
@@ -9,37 +9,54 @@ import faker from 'faker'
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Marcus"
-        time="15 minutes ago"
-        avatar={faker.image.avatar()}  commentText="Cool!"
-      />
-      <CommentDetail
-        author="Adrian"
-        time="22 minutes ago"
-        avatar={faker.image.avatar()} commentText="Nice stuff!"
-      />
-      <CommentDetail
-        author="Lucille"
-        time="38 minutes ago"
-        avatar={faker.image.avatar()} commentText="Dig it!"
-      />
-      <CommentDetail
-        author="Jerome"
-        time="58 minutes ago"
-        avatar={faker.image.avatar()} commentText="Cool!"
-      />
-      <CommentDetail
-        author="Marnice"
-        time="4 hours ago"
-        avatar={faker.image.avatar()} commentText="Dope!"
-      />
-      <CommentDetail
-        author="Alex"
-        time="2 days ago"
-        avatar={faker.image.avatar()} commentText="Cool!"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          You ate my cookies. Don't do that!
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Marcus"
+          time="15 minutes ago"
+          avatar={faker.image.avatar()}  commentText="Cool!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Adrian"
+          time="22 minutes ago"
+          avatar={faker.image.avatar()} commentText="Nice stuff!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Lucille"
+          time="38 minutes ago"
+          avatar={faker.image.avatar()} commentText="Dig it!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jerome"
+          time="58 minutes ago"
+          avatar={faker.image.avatar()} commentText="Cool!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Marnice"
+          time="4 hours ago"
+          avatar={faker.image.avatar()} commentText="Dope!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          time="2 days ago"
+          avatar={faker.image.avatar()} commentText="Cool!"
+        />
+      </ApprovalCard>
     </div>
   )
 }
